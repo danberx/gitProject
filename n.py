eb386acfb03ec50290b72641a28d1cc9c64d050f
danbe_2uptnw8 <danberx490@gmail.com>
2022-11-17 19:38:53 +0300

    we need more print

--- n.py
+++ n.py
@@ -2,6 +2,7 @@ def main():
     def main():
         print('My first git program')
         print('And I change it every day')
+        print("abcd")
 
     if __name__ == '__main__':
         main()
